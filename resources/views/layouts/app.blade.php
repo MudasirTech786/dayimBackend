6e0c0b7c3d9f5b4c885789884a35bdecb4c5ba07
Your Name~ <you@example.com>
2024-03-26 08:56:35 +0500

    remove tailwind from project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -11,10 +10,7 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">    
 </head>
 <body>
     <div id="app">
