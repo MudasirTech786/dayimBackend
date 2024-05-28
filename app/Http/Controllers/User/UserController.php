e608d4917f4f609289d139e6407eb75c269f41c1
MudasirTech786 <m.mudasirasfi@gmail.com>
2024-05-28 17:07:36 +0500

    --

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -84,7 +84,7 @@ class UserController extends Controller
 
         $result = User::where('name', '!=', 'Admin')->orderBy('created_at', 'DESC');
 
-        $aColumns = ['name', 'email', 'id_card', 'phone', 'role'];
+        $aColumns = ['name', 'email', 'cnic', 'phone'];
 
         $iStart = $request->get('iDisplayStart');
         $iPageSize = $request->get('iDisplayLength');
@@ -118,7 +118,7 @@ class UserController extends Controller
             $result->Where(function ($query) use ($sKeywords) {
                 $query->orWhere('name', 'LIKE', "%{$sKeywords}%");;
                 $query->orWhere('email', 'LIKE', "%{$sKeywords}%");
-                $query->orWhere('id_card', 'LIKE', "%{$sKeywords}%");
+                $query->orWhere('cnic', 'LIKE', "%{$sKeywords}%");
                 $query->orWhere('phone', 'LIKE', "%{$sKeywords}%");
             });
         }
