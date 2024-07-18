610cac400d14d785b05edb46ac1f06cb11cedfb1
Your Name~ <you@example.com>
2024-07-18 09:18:53 +0500

    ...

--- resources/views/admin/user/view.blade.php
+++ resources/views/admin/user/view.blade.php
@@ -76,7 +76,7 @@
 @section('content')
     <div class="content-body">
 
-        <section id="configuration">
+        {{-- <section id="configuration">
             <div class="row">
                 <div class="col-12">
                     <div class="card">
@@ -100,7 +100,7 @@
                                     <thead>
                                         <tr>
                                             <th>Name</th>
-                                            <th>Email</th>
+                                            <th style="width:1500px !important">Emaill</th>
                                             <th>ID Card</th>
                                             <th>Phone</th>
                                             <th>Roles</th>
@@ -110,6 +110,50 @@
                                     <tbody>
                                     </tbody>
                                 </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section> --}}
+
+
+        <section id="configuration">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Users Contact Details</h4>
+                            <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                            <div class="heading-elements">
+                                <ul class="list-inline mb-0">
+                                    <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                                    <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                                    <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                                    <li><a data-action="close"><i class="ft-x"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="card-content collapse show ">
+                            <div class="card-body card-dashboard ">
+                                <p class="card-text"></p>
+                                <div class="overflow-auto">
+                                    <table class="table table-striped table-bordered zero-configuration data-table "
+                                        id="link_table">
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th style="">Emaill</th>
+                                                <th>ID Card</th>
+                                                <th>Phone</th>
+                                                <th>Roles</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
